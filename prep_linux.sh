e2e5cb6db41c235985023c38239841eb67f20de5
Geo Pertea <geo.pertea@gmail.com>
2015-04-08 15:36:00 -0400

    added deploy prep scripts

---
+++ prep_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ver=$(fgrep '#define VERSION ' stringtie.cpp)
+ver=${ver#*\"}
+ver=${ver%%\"*}
+pack=stringtie-$ver
+linpack=$pack.Linux_x86_64
+echo "preparing $linpack.tar.gz"
+echo "-------------------"
+/bin/rm -rf $linpack
+/bin/rm -f $linpack.tar.gz
+mkdir $linpack
+make clean
+make release
+cp -p LICENSE README stringtie $linpack/
+tar cvfz $linpack.tar.gz $linpack
+ls -l $linpack.tar.gz
+echo "If you're on igmN machines you can also update the web files:"
+echo "cp $linpack.tar.gz $pack.tar.gz  ~/html/software/stringtie/dl/"
+echo "perl -i -pe 's/stringtie\-\d\.\d+\.\d+\./stringtie-$ver./g' ~/html/software/stringtie/index.shtml"
---
+++ prep_source.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+ver=$(fgrep '#define VERSION ' stringtie.cpp)
+ver=${ver#*\"}
+ver=${ver%%\"*}
+pack=stringtie-$ver
+echo "preparing $pack.tar.gz"
+echo "-------------------"
+/bin/rm -rf $pack
+/bin/rm -f $pack.tar.gz
+mkdir $pack
+mkdir $pack/gclib
+cd samtools-0.1.18
+make clean
+cd ..
+libdir=stringtie-$ver/gclib/
+cp -p Makefile LICENSE README stringtie.cpp tablemaker.{h,cpp} rlink.{h,cpp} $pack/
+#sed 's|\.\./gclib|./gclib|' Makefile > $pack/Makefile
+cp -pr samtools-0.1.18 $pack/
+/bin/rm -rf $pack/samtools-0.1.18/.svn
+cp -p ./gclib/{GVec,GList,GHash}.hh $libdir
+cp -p ./gclib/GBitVec.h $libdir
+cp -p ./gclib/{GArgs,GBam,GBase,gdna,GStr,gff,codons,GFaSeqGet,proc_mem,GThreads}.{h,cpp} $libdir
+tar cvfz $pack.tar.gz $pack
+ls -l $pack.tar.gz
+#scp $pack.tar.gz igm3:~/src/
