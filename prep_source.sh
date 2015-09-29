b82f8788f7cf8f3651a3a728718399e21b09f99e
Geo Pertea <geo.pertea@gmail.com>
2015-09-29 15:04:59 -0400

    adding GIntHash.hh

--- prep_source.sh
+++ prep_source.sh
@@ -17,7 +17,7 @@ cp -p Makefile LICENSE README stringtie.cpp tablemaker.{h,cpp} rlink.{h,cpp} $pa
 #sed 's|\.\./gclib|./gclib|' Makefile > $pack/Makefile
 cp -pr samtools-0.1.18 $pack/
 /bin/rm -rf $pack/samtools-0.1.18/.svn
-cp -p ./gclib/{GVec,GList,GHash}.hh $libdir
+cp -p ./gclib/{GVec,GList,GHash,GIntHash}.hh $libdir
 cp -p ./gclib/GBitVec.h $libdir
 cp -p ./gclib/{GArgs,GBam,GBase,gdna,GStr,gff,codons,GFaSeqGet,proc_mem,GThreads}.{h,cpp} $libdir
 tar cvfz $pack.tar.gz $pack
