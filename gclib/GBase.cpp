f2d36c434f1260630c1da5bc81fd9374b361a677
Geo Pertea <geo.pertea@gmail.com>
2020-08-24 17:54:22 -0400

    strcasecmp fix for cygwin

--- gclib/GBase.cpp
+++ gclib/GBase.cpp
@@ -239,12 +239,40 @@ FILE* Gfopen(const char *path, char *mode) {
 
 bool GstrEq(const char* a, const char* b) {
 	 if (a==NULL || b==NULL) return false;
-	 return (strcmp(a,b)==0);
+	 return (strcmp(a, b)==0);
 }
+
+
+#ifdef __CYGWIN__
+int strcasecmp (const char *s1, const char *s2) {
+  int d = 0;
+  for ( ; ; ) {
+     const int c1 = tolower(*s1++);
+     const int c2 = tolower(*s2++);
+     if (((d = c1 - c2) != 0) || (c2 == '\0'))
+       break;
+  }
+  return d;
+}
+
+int strncasecmp (const char *s1, const char *s2,
+                  size_t n) {
+  int d = 0;
+  for ( ; n != 0; n--) {
+     const int c1 = tolower(*s1++);
+     const int c2 = tolower(*s2++);
+     if (((d = c1 - c2) != 0) || (c2 == '\0'))
+       break;
+  }
+  return d;
+}
+
+#endif
+
 
 bool GstriEq(const char* a, const char* b) {
 	 if (a==NULL || b==NULL) return false;
-	 return (strcasecmp(a,b)==0);
+	 return (strcasecmp(a, b)==0);
 }
 
 int Gstricmp(const char* a, const char* b, int n) {
