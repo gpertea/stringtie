c0aa561afed89af50e32dfd4c1ff5777bf5f338a
Geo Pertea <geo.pertea@gmail.com>
2021-03-09 17:49:41 -0500

    fixed v2.1.5 release

--- tag_git.sh
+++ tag_git.sh
@@ -2,6 +2,13 @@
 ver=$(fgrep '#define VERSION ' stringtie.cpp)
 ver=${ver#*\"}
 ver=${ver%%\"*}
+if [[ "$1" == "delete" || "$1" == "del" ]]; then
+  echo "Deleting tag v$ver .."
+  git tag -d v$ver
+  git push origin :refs/tags/v$ver
+  exit
+fi
+echo "Tagging with v$ver"
 git checkout master
 git tag -a "v$ver" -m "release $ver"
 git push --tags
