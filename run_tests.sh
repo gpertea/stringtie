b365c3775ae55d3ecc98d8747894ed26426b7722
Geo Pertea <geo.pertea@gmail.com>
2024-02-08 19:56:25 -0500

    added basic test for short reads with guides

--- run_tests.sh
+++ run_tests.sh
@@ -33,12 +33,12 @@ function unpack_test_data() {
 cd tests
 # array element format:
 # 
-arrins=("short_reads" "short_reads_and_superreads" "long_reads" "long_reads" \
-  "mix_short mix_long" "mix_short mix_long")
-arrparms=("" "" "-L" "-L -G human-chr19_P.gff" "--mix" "--mix -G mix_guides.gff")
-arrout=("short_reads" "short_reads_and_superreads" "long_reads" "long_reads_guided" \
-  "mix_reads" "mix_reads_guided")
-arrmsg=("Short reads"  "Short reads and super-reads" "Long reads" \
+arrins=("short_reads" "short_reads_and_superreads" "mix_short" "long_reads" \
+   "long_reads" "mix_short mix_long" "mix_short mix_long")
+arrparms=("" "" "-G mix_guides.gff" "-L" "-L -G human-chr19_P.gff" "--mix" "--mix -G mix_guides.gff")
+arrout=("short_reads" "short_reads_and_superreads" "short_guided" "long_reads" \
+       "long_reads_guided" "mix_reads" "mix_reads_guided")
+arrmsg=("Short reads"  "Short reads and super-reads" "Short reads with annotation guides" "Long reads" \
  "Long reads with annotation guides" "Mixed reads" "Mixed reads with annotation guides")
 for i in ${!arrmsg[@]}; do
  fout="${arrout[$i]}.out.gtf"
@@ -54,9 +54,11 @@ for i in ${!arrmsg[@]}; do
  fin=${arrins[$i]}.bam
  if [[ ${arrins[$i]} =~ ^mix ]]; then
    ins=( ${arrins[$i]} )
-   fin="${ins[0]}.bam ${ins[1]}.bam"
+   if [[ ${#ins[@]} -gt 1 ]]; then
+     fin="${ins[0]}.bam ${ins[1]}.bam"
+   fi
  fi
-
+ echo "Running: ../stringtie ${arrparms[$i]} -o $fout $fin"
  ../stringtie ${arrparms[$i]} -o $fout $fin
  if [ ! -f $fout ]; then
    echo "Error: file $fout not created! Failed running stringtie on $fin"
